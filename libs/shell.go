3b4376473c6abef696e5acf43fde5891febbfe67
Игорь (JS) <igor.js@hitech18.online>
2019-08-13 12:27:02 +0300

    Shell. Add workdir for cmd.

--- libs/shell.go
+++ libs/shell.go
@@ -8,12 +8,13 @@ import (
 	"time"
 )
 
-func ExecCommandWithTimeLimit(timeLimit time.Duration, base string, args ...string) ([]byte, error) {
+func ExecCommandWithTimeLimit(timeLimit time.Duration, workDir string, base string, args ...string) ([]byte, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), timeLimit)
 	defer cancel()
 
 	out := []byte("")
 	cmd := exec.CommandContext(ctx, base, args...)
+	cmd.Dir = workDir
 	out, err := cmd.CombinedOutput()
 
 	if ctx.Err() == context.DeadlineExceeded {
