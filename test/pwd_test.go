62314ca365fac1eeb197fb3bc1f34f84de432061
igor.js <igor.js@hitech18.online>
2019-11-25 18:44:06 +0200

    Add pass gene.

--- test/pwd_test.go
+++ test/pwd_test.go
@@ -11,12 +11,16 @@ import (
 func TestTools(t *testing.T) {
 	Convey("Gene password", t, func() {
 		opt := lib.PasswordGeneOptions{}
+		opt.Digits = "1"
+
 		pass := string(lib.GenePassword(&opt))
 
 		fmt.Printf("\n pass is %v \n", pass)
 
 		So(len(pass), ShouldEqual, lib.PwdGeneCharsMinDefault)
 		opt.Length = lib.PwdGeneCharsMin
+
+
 		pass = string(lib.GenePassword(&opt))
 		So(len(pass), ShouldEqual, lib.PwdGeneCharsMin)
 
@@ -25,22 +29,26 @@ func TestTools(t *testing.T) {
 		fmt.Printf("\n pass is %+v \n", pass)
 
 		for _, val := range pass {
-			if strings.Index(opt.Up, string(val)) > -1 {
+
+			charStr := string(val)
+
+			if strings.Index(opt.Up, charStr) > -1 {
 				res.Up++
 				continue;
 			}
 
-			if strings.Index(opt.Down, string(val)) > -1 {
+			if strings.Index(opt.Down, charStr) > -1 {
 				res.Down++
 				continue;
 			}
 
-			if strings.Index(opt.Digits, string(val)) > -1 {
+			if strings.Index(opt.Digits, charStr) > -1 {
+				So(charStr, ShouldEqual, "1")
 				res.Digit++
 				continue;
 			}
 
-			if strings.Index(opt.Specials, string(val)) > -1 {
+			if strings.Index(opt.Specials, charStr) > -1 {
 				res.Special++
 				continue;
 			}
